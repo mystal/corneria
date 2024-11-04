a647793f55f578d094c9c8ba33041742e39f83fa
Gabriel Martinez <reitaka@gmail.com>
2024-11-03 17:52:59 -0800

    Add a way to make enemies not shoot

--- godot/entities/enemies/enemy.gd
+++ godot/entities/enemies/enemy.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export_group("Shooter")
 @export var projectile_scene: PackedScene
 @export var fire_rate: float = 2.0
+@export var wants_to_fire: bool = true
 
 var faction: Enums.Faction = Enums.Faction.ENEMY
 
@@ -11,7 +12,7 @@ var faction: Enums.Faction = Enums.Faction.ENEMY
 
 func _physics_process(delta: float) -> void:
 	_fire_cooldown -= delta
-	if _fire_cooldown <= 0.0:
+	if wants_to_fire and _fire_cooldown <= 0.0:
 		# Spawn projectile
 		var new_projectile: Projectile = projectile_scene.instantiate()
 		new_projectile.instigator = self
